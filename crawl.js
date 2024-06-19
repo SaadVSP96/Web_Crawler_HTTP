785fc1a9ce614c21cff10914d9fadf6d73074c8d
SaadVSP96 <saadmasood192@gmail.com>
2024-06-19 13:12:58 +0500

    3. Added JSDOM as dependance for parsing HTML

--- crawl.js
+++ crawl.js
@@ -1,8 +1,18 @@
 // create a file crawl.js
 // Inside the file, define the funciton stub
 
-function normalizeURL(url){
-    // immplementation pending
+function normalizeURL(urlString){
+    // Parse the URL
+    const url = new URL(urlString);
+
+    // Combine the Hostname and pathname
+    let normalized = `${url.hostname}${url.pathname}`;
+
+    // Remove the trailing slash if its not the only character in the pathname
+    if (normalized.endsWith('/') && normalized !== `${url.hostname}/`){
+        normalized = normalized.slice(0, -1);
+    }
+    return normalized.toLowerCase();
 }
 
 // expoting the function:
