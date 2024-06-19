b586f253a86626f931901188dc22710700028ad8
SaadVSP96 <saadmasood192@gmail.com>
2024-06-19 16:07:26 +0500

    4. added a getURLsFromHTML function and improving it  incrementally

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
-// create a file crawl.js
-// Inside the file, define the funciton stub
+// importing the requisite classes
+const { JSDOM } = require('jsdom')
+
+// this function allows the web crawler to extract the 
+// links from the webpage's full HTML
+// the HTML will be an obvious first input
+// the baseURL is the URL of the website we are in the 
+// process of crawling. there is the obvious recursive 
+// potential here as well as internal links get reused 
+// base URLs
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements){
+        // check if its a relative URL
+        if (linkElement.href.slice(0,1) === '/'){
+            // relative URL
+            urls.push(`${baseURL}${linkElement.href}`)
+        }else{
+            // absolute URL
+            urls.push(linkElement.href)
+        }
+    }
+    return urls 
+}
 
 function normalizeURL(urlString){
     // Parse the URL
@@ -15,5 +39,8 @@ function normalizeURL(urlString){
     return normalized.toLowerCase();
 }
 
-// expoting the function:
-export { normalizeURL };
+// this is how you export stuff for CJS - common JavaScript
+module.exports = {
+    normalizeURL,
+    getURLsFromHTML,
+}
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,8 @@
-import { test, expect } from '@jest/globals';
-import { normalizeURL } from './crawl.js';
+// this is how you import stuff for CJS - common JavaScript
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
 
+// Tests for the normalizeURL function:
 test('normalizeURL strips protocol', () => {
     const input = 'https://blog.boot.dev/path/';
     const expected = 'blog.boot.dev/path';
@@ -47,3 +49,83 @@ test('normalizeURL handles query parameters', () => {
     actual = normalizeURL(input);
     expect(actual).toEqual(expected);
 });
+
+
+// Tests for the getURLsFromHTML function:
+// An absolute URL has protocol and domain in addition to path
+test('getURLsFromHTML gets absolute URLs ', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href = "https://blog.boot.dev/path/">
+            Boot.dev Blog
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://blog.boot.dev/path/";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.boot.dev/path/"];
+    expect(actual).toEqual(expected);
+});
+
+// A Relative URL does not have protocol and domain, but only path
+// the relative URL here is the same as the absolute URL in above test.
+// the browser, if it finds a relative URL like /path/ will assume that the 
+// same domain as the page preceeds the first slash in /path/
+
+// we dont however want a relative URL but we want to extract it as a full
+// absolute URL. This will allow requests to be made to the extracted URL later.
+test('getURLsFromHTML gets relative URLs ', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href = "/path/">
+            Boot.dev Blog
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.boot.dev/path/"];
+    expect(actual).toEqual(expected);
+});
+
+// Now lets have it do both a relative and an absolute URL
+test('getURLsFromHTML gets relative and absolute URLs ', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href = "/path1/">
+            Boot.dev Blog
+        </a>
+        <a href = "https://blog.boot.dev/path2/">
+            Boot.dev Blog
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"];
+    expect(actual).toEqual(expected);
+});
+
+// Finally, lets make sure the code doesn't include a bad URL into the array.
+test('getURLsFromHTML gets Invalid URLs ', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href = "invalid">
+            Invalid URL
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [];
+    expect(actual).toEqual(expected);
+});
+
